2c2e0cbf2c49563e50601706878157e319039b06
sd1700092 <foreverskyone@hotmail.com>
2019-04-12 17:34:35 +0800

    Initial commit

---
+++ src/main/java/utils/movieYearRegex.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 作者：blogchong
+ * 公众号: 数据虫巢(ID:blogchong)
+ * 交流微信：mute88
+ * Desc: 抽取年份公式
+ */
+public class movieYearRegex {
+
+    private  static String moduleType = ".* \\(([1-9][0-9][0-9][0-9])\\).*";
+
+    public static void main(String[] args){
+        System.out.println(movieYearReg("GoldenEyes (1999)"));
+    }
+
+    public static int movieYearReg(String str){
+
+        int retYear = 1994;
+        Pattern patternType = Pattern.compile(moduleType);
+        Matcher matcherType = patternType.matcher(str);
+        while (matcherType.find()) {
+            retYear = Integer.parseInt(matcherType.group(1));
+        }
+        return retYear;
+    }
+
+}
